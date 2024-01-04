522b5a1b4788f8b79c4d18222755fa5573f0bcc9
luisOtarola <forexdoc2710@gmail.com>
2024-01-04 15:08:00 -0300

    activiy preference and  harcode fidex

--- app/src/main/java/com/example/bop_it1/PreferenceActivity.kt
+++ app/src/main/java/com/example/bop_it1/PreferenceActivity.kt
@@ -2,10 +2,12 @@ package com.example.bop_it1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class PreferenceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preference)
+
     }
 }
+
+
