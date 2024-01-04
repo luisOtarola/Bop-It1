bfcc3111888d7d5ef1232e437adfae184baed2ff
luisOtarola <forexdoc2710@gmail.com>
2024-01-03 22:33:22 -0300

    activities created

---
+++ app/src/main/java/com/example/bop_it1/AboutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bop_it1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
